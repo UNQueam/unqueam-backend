3ad7ab77961dff36b58839f46f9c835bc0211528
Nicolas De Maio <nicolas.demaio19@gmail.com>
2023-08-26 11:28:02 -0300

    Add initial scaffolding

---
+++ src/main/kotlin/com/unqueam/gamingplatform/GamingPlatformApplication.kt
@@ -0,0 +1,11 @@
+package com.unqueam.gamingplatform
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GamingPlatformApplication
+
+fun main(args: Array<String>) {
+    runApplication<GamingPlatformApplication>(*args)
+}
---
+++ src/test/kotlin/com/unqueam/gamingplatform/GamingPlatformApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.unqueam.gamingplatform
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GamingPlatformApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
