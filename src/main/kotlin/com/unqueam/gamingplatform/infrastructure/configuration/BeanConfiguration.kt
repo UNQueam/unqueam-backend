fb7ce19ab42adad650ed8eb89aedf33fea77793f
Nicolas De Maio <nicolas.demaio19@gmail.com>
2023-08-26 21:32:59 -0300

    Finish project structure

---
+++ src/main/kotlin/com/unqueam/gamingplatform/infrastructure/configuration/BeanConfiguration.kt
@@ -0,0 +1,18 @@
+package com.unqueam.gamingplatform.infrastructure.configuration
+
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class BeanConfiguration {
+
+    // TODO: Bean initializations.
+    /*
+    Example:
+
+    @Bean
+    OrderService orderService(OrderRepository orderRepository) {
+        return new DomainOrderService(orderRepository);
+    }
+     */
+
+}
