44a7dd07def382096b168c4cf3ee7559da11182f
Nicolas De Maio <nicolas.demaio19@gmail.com>
2023-09-03 01:53:50 -0300

    Fix ranks names

--- src/main/kotlin/com/unqueam/gamingplatform/core/domain/RankBadge.kt
+++ src/main/kotlin/com/unqueam/gamingplatform/core/domain/RankBadge.kt
@@ -1,13 +1,13 @@
 package com.unqueam.gamingplatform.core.domain
 
 enum class RankBadge(val rankName: String, val  nextRank: RankBadge, val neededViews: Long) {
-    GOLD("rank1", GOLD, 100L) {
+    GOLD("gold", GOLD, 100L) {
         override fun shouldMoveUpToTheNextRank(gameViewsEvents: Long): Boolean {
             return false
         }
     },
-    SILVER("rank2", GOLD, 50L),
-    BRONZE("rank3", SILVER, 15L),
+    SILVER("silver", GOLD, 50L),
+    BRONZE("bronze", SILVER, 15L),
     UNRANKED("unranked", BRONZE, 0L);
 
     open fun shouldMoveUpToTheNextRank(gameViewsEvents: Long): Boolean {
