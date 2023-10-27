18d16afc47cff45f0165c30feaecb2b88a5a491f
Nicolas De Maio <nicolas.demaio19@gmail.com>
2023-10-26 23:11:02 -0300

    :hammer: remove unnecessary changes

--- src/main/kotlin/com/unqueam/gamingplatform/application/exception/APIExceptionHandler.kt
+++ src/main/kotlin/com/unqueam/gamingplatform/application/exception/APIExceptionHandler.kt
@@ -15,14 +15,8 @@ import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.core.userdetails.UsernameNotFoundException
-import org.springframework.validation.FieldError
-import org.springframework.validation.ObjectError
-import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.ResponseStatus
-import java.util.function.Consumer
-
 
 @ControllerAdvice
 class APIExceptionHandler {
@@ -86,22 +80,6 @@ class APIExceptionHandler {
     @ExceptionHandler(InvalidCommentContentException::class)
     fun handleInvalidCommentContentException(exception: InvalidCommentContentException, httpRequest: HttpServletRequest): ResponseEntity<Map<String, Any>> {
         return buildErrorResponse(HttpStatus.BAD_REQUEST, exception, httpRequest, exception.errorsMap)
-    }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(
-        MethodArgumentNotValidException::class
-    )
-    fun handleValidationExceptions(
-        ex: MethodArgumentNotValidException
-    ): Map<String, String?> {
-        val errors: MutableMap<String, String?> = HashMap()
-        ex.bindingResult.allErrors.forEach(Consumer { error: ObjectError ->
-            val fieldName = (error as FieldError).field
-            val errorMessage = error.getDefaultMessage()
-            errors[fieldName] = errorMessage
-        })
-        return errors
     }
 
     private fun buildErrorResponse(httpStatus: HttpStatus, exception: RuntimeException, httpRequest: HttpServletRequest, errorsMap: Map<String, Any> = mapOf()) : ResponseEntity<Map<String, Any>> {
