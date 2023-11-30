397e070290da755c307b219b678c74fcb7b74aa5
Nicolas De Maio <nicolas.demaio19@gmail.com>
2023-11-30 20:04:47 -0300

    add total to metrics count (#51)

--- src/main/kotlin/com/unqueam/gamingplatform/core/tracking/DailyMetricsReport.kt
+++ src/main/kotlin/com/unqueam/gamingplatform/core/tracking/DailyMetricsReport.kt
@@ -2,9 +2,11 @@ package com.unqueam.gamingplatform.core.tracking
 
 class DailyMetricsReport {
 
+    val total: Long
     val result: List<Map<String, Any>>
 
-    constructor(result: List<Map<String, Any>>) {
+    constructor(totalCount: Long, result: List<Map<String, Any>>) {
         this.result = result
+        this.total = totalCount
     }
 }
