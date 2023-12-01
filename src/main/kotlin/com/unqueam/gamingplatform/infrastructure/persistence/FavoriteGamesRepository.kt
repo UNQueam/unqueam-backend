bb86742bac38e54e347e62326883bc2ab65a6d0c
Nicolas De Maio <nicolas.demaio19@gmail.com>
2023-11-30 23:01:38 -0300

    no fetch favorite games if hidden (#52)

--- src/main/kotlin/com/unqueam/gamingplatform/infrastructure/persistence/FavoriteGamesRepository.kt
+++ src/main/kotlin/com/unqueam/gamingplatform/infrastructure/persistence/FavoriteGamesRepository.kt
@@ -7,7 +7,11 @@ import java.util.Optional
 
 interface FavoriteGamesRepository : JpaRepository<FavoriteGame, Long> {
 
-    @Query ("select favGame from FavoriteGame favGame INNER JOIN PlatformUser user ON favGame.platformUser.id = user.id where user.id = :userId")
+    @Query ("select favGame from FavoriteGame favGame " +
+            "INNER JOIN PlatformUser user " +
+            "ON favGame.platformUser.id = user.id " +
+            "where user.id = :userId " +
+            "AND favGame.game.isHidden = false")
     fun findAllByPlatformUser(userId: Long): List<FavoriteGame>
 
     @Query ("from FavoriteGame favGame where favGame.game.id = :gameId")
